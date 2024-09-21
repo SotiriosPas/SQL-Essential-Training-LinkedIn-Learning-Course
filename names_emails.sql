59fa4a81dcd89fdb6ad680c3c2f85b2cadead179
Sotirios Paschalis <109233140+SotiriosPas@users.noreply.github.com>
2024-09-21 18:28:28 +0300

    Added practice query

---
+++ names_emails.sql
@@ -0,0 +1,14 @@
+/*
+CREATED BY: SOTIRIOS PASCHALIS
+CREATE DATE: 21/09/2024
+DESCRIPTION: This query displays all customers first names, last names and email addresses
+*/
+
+SELECT 
+	FirstName,
+	LastName,
+	Email
+FROM 
+	Customer
+
+
